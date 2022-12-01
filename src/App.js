e4501b2cfeb5043eee97e4bc8478d9325330df63
sushant-sg9 <sushantghadge2016@gmail.com>
2022-12-01 18:22:12 +0530

    search image app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [photo , setPhoto] = useState("")
+  const [image , setImage] = useState([])
+  const changeImage = () =>{
+    axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo} &client_id=Qy0LZ7_P6LHz8-Csw5VbLB-L4j_wqjk3fD7YthA1M-A`)
+     .then((response)=>{
+      setImage(response.data.results);
+     })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+      <input type="text" placeholder='Search Images'  className='searchImage' value={photo} onChange={(e)=>{
+      setPhoto(e.target.value)
+      }} />
+      <button type='submit' onClick={changeImage}>Images</button>
     </div>
+      <div className='container2'>
+        {image.map((value)=>{
+          return(
+            <div>
+              <img src={value.urls.small} alt=''/>
+            </div>
+          )
+        })}
+      </div>
+    </>
   );
 }
 
